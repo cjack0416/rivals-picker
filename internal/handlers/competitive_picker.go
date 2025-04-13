04283a15a1cf33635b7b6939ca9670bff63f6930
Chris Jackson <chrisjackson0416@gmail.com>
2025-04-12 18:52:07 -0700

    Adding filterHeroRoles function

--- internal/handlers/competitive_picker.go
+++ internal/handlers/competitive_picker.go
@@ -1,23 +1,82 @@
 package handlers
 
 import (
+	"context"
 	"strings"
 
 	"github.com/cjack0416/rivals-picker/api"
+	"github.com/cjack0416/rivals-picker/internal/tools"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
+type Hero struct {
+	HeroId int
+	HeroName string
+	HeroRole string
+	HeroScore int
+}
+
+const duelist = "DUELIST"
+const vanguard = "VANGUARD"
+const strategist = "STRATEGIST"
+
 func CompetitivePicker(c *fiber.Ctx) error {
+	conn := tools.GetDatabaseConn()
+
 	var params = api.PickHeroesCompetitiveParams{}
 
 	m := c.Queries()
 	params.TeamHeroes = strings.Split(m["team_heroes"], ",")
 	params.EnemyHeroes = strings.Split(m["enemy_heroes"], ",")
 
-	log.Info("Your teams heroes are: ", params.TeamHeroes)
-	log.Info("Your first team hero is: ", params.TeamHeroes[0])
-	log.Info("The enemy heroes are: ", params.EnemyHeroes)
+	log.Info("Making query in heroes table")
+	rows, err := conn.Query(context.Background(), "SELECT heroes.hero_id, heroes.hero_name, roles.role_name FROM heroes LEFT JOIN roles ON heroes.role_id = roles.role_id")
+	if err != nil {
+		log.Errorf("Error querying heroes from heroes table with query params team_heroes=%s, and error=%s", params.TeamHeroes, err)
+		return c.Status(500).JSON("{}")
+	}
+	log.Info("Successfully made query from heroes table")
 
-	return c.Status(200).JSON("{}")
+	var allHeroes map[string]Hero = make(map[string]Hero)
+	for rows.Next() {
+		var hero = Hero{}
+		err := rows.Scan(&hero.HeroId, &hero.HeroName, &hero.HeroRole)
+		if err != nil {
+			log.Error("Error scanning row: ", err)
+			return c.Status(500).JSON("{}")
+		}
+		allHeroes[hero.HeroName] = hero
+	}
+	log.Info("Result from heroes table query: ", allHeroes)
+
+	var heroPicks map[string]Hero = filterHeroRoles(allHeroes, params.TeamHeroes)
+
+	return c.Status(200).JSON(heroPicks)
+}
+
+func filterHeroRoles(heroMap map[string]Hero, teamHeroes []string) map[string]Hero {
+	var filterRoles map[string]int = map[string]int{duelist: 0, vanguard: 0, strategist: 0}
+
+	for _, hero := range teamHeroes {
+		heroRole := heroMap[hero].HeroRole
+		if heroRole == duelist {
+			filterRoles[duelist]++
+		} else if heroRole == vanguard {
+			filterRoles[vanguard]++
+		} else {
+			filterRoles[strategist]++
+		}
+	}
+
+	var filteredHeroMap map[string]Hero = make(map[string]Hero)
+
+	for _, hero := range heroMap {
+		if filterRoles[hero.HeroRole] < 3 {
+			hero.HeroScore += 3 - filterRoles[hero.HeroRole]
+			filteredHeroMap[hero.HeroName] = hero
+		}
+	}
+
+	return filteredHeroMap
 }
