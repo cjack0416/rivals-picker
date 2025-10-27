f14baf761c8584d37ea0f1141b11f5b8afcfa123
cjack0416 <chrisjackson0416@gmail.com>
2025-10-26 23:06:24 -0700

    AbilityCounterEngine initial implementation

--- internal/engines/engines.go
+++ internal/engines/engines.go
@@ -1,18 +1,38 @@
 package engines
 
-import "github.com/cjack0416/rivals-picker/internal/model"
+import (
+	"fmt"
 
-type Engine[T model.EngineQueryRow] interface {
-	Run(heroPicks []model.Hero) []model.Hero
+	"github.com/cjack0416/rivals-picker/internal/model"
+	"github.com/cjack0416/rivals-picker/internal/tools"
+)
+
+type Engine[T model.EngineQueryResult] interface {
+	Run(heroPicks []model.Hero, args [][]int) ([]model.Hero, error)
 	addPickReason(rows T)
 }
 
 type AbilityCounterEngine struct {}
 
-func (ac AbilityCounterEngine) Run(heroPicks []model.Hero) []model.Hero {
-	return []model.Hero{}
+func (ac AbilityCounterEngine) Run(heroPicks []model.Hero, args [][]int) ([]model.Hero, error) {
+	var heroIdList = make([]int, 0, len(heroPicks))
+
+	for _, hero := range heroPicks {
+		heroIdList = append(heroIdList, hero.HeroId)
+	}
+	acRows, err := tools.GetHeroAbilityCounters(heroIdList, args[0])
+	if err != nil {
+		return nil, err
+	}
+
+	for _, row := range acRows {
+		fmt.Println(row)
+	}
+
+
+	return []model.Hero{}, nil
 }
 
-func (ac AbilityCounterEngine) addPickReason(rows model.AbilityCounterRow) {
+func (ac AbilityCounterEngine) addPickReason(rows model.AbilityCounterResult) {
 
 }
